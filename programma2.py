3b4bf2364cdb5e8cf44c8163b06f34b70be7f67f
mindthenap <laura.rossi191@gmail.com>
2020-04-26 19:44:15 +0200

    programma 2 - bigrammi più freq terminato

--- programma2.py
+++ programma2.py
@@ -34,7 +34,7 @@ def tokenizePOS(frasi):
 # crea lista token senza la punteggiatura
 def tokNoPunct(tokensList):
     tokListNoPunct = []
-    punct = [".", ",", ":", ";", "!", "?", "/", "..", "-", "(", ")"]
+    punct = [".", ",", ":", ";", "!", "?", "/", "..", "-", "(", ")", "'"]
     for token in tokensList:
         if token not in punct:
             tokListNoPunct.append(token)
@@ -72,6 +72,23 @@ def mostFreqNomiAgg(tokensPOS):
     return mostFreqNomi, mostFreqAgg
 
 
+# toglie dalla lista dei POS punteggiatura, congiunzioni e articoli
+def noPuntArtCong(tokensPOS):
+    # creo lista bigrammi di POS
+    listaBigrammi = nltk.bigrams(tokensPOS)
+    # prendo tag punteggiatura, congiunzioni e articoli da https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
+    listaTagDaEscludere = ["DT", "CC", "IN", "SYM", ".", "!", "?", ",", "(", ")", ":", ";", "-", "_", "'"]
+    # creo nuova lista bigrammi i cui tag non sono tra quelli da escludere
+    listaBigrNew = []
+    for ((tok1, tag1), (tok2, tag2)) in listaBigrammi:
+        if tag1 not in listaTagDaEscludere:
+            if tag2 not in listaTagDaEscludere:
+                bigramma = (tok1,tok2)
+                listaBigrNew.append(bigramma)
+
+    return listaBigrNew
+
+
 def main(file1, file2):
     # metodo open per aprire file di testo
     fileInput1 = codecs.open(file1, "r", "utf-8")
@@ -98,8 +115,12 @@ def main(file1, file2):
     tok20MostFreq1 = tok20PiuFreq(tokNoPunct1)
     tok20MostFreq2 = tok20PiuFreq(tokNoPunct2)
     print "20 token più frequenti in ordine di frequenza decrescente:"
-    print "\nRECENSIONI POSITIVE:\n", tok20MostFreq1
-    print "\nRECENSIONI NEGATIVE:\n", tok20MostFreq2
+    print "\nRECENSIONI POSITIVE:\nToken:", "\tFrequenza:"
+    for token in tok20MostFreq1:
+        print token[0], "\t", token[1]
+    print "\nRECENSIONI NEGATIVE:\nToken:", "\tFrequenza:"
+    for token in tok20MostFreq2:
+        print token[0], "\t", token[1]
 
     # sostantivi e aggettivi più frequenti
     sostMostFreq1, aggMostFreq1 = mostFreqNomiAgg(tokensPOS1)
@@ -108,13 +129,35 @@ def main(file1, file2):
     sostMostFreq1 = tok20PiuFreq(sostMostFreq1)
     sostMostFreq2 = tok20PiuFreq(sostMostFreq2)
     print "\n20 sostantivi più frequenti in ordine di frequenza decrescente:"
-    print "\nRECENSIONI POSITIVE:", sostMostFreq1, "\n"
-    print "RECENSIONI NEGATIVE:", sostMostFreq2, "\n"
+    print "\nRECENSIONI POSITIVE:"
+    for sostantivo in sostMostFreq1:
+        print sostantivo[0], sostantivo[1]
+    print "\nRECENSIONI NEGATIVE:"
+    for sostantivo in sostMostFreq2:
+        print sostantivo[0], sostantivo[1]
     # prendo solo i primi 20 aggettivi
     aggMostFreq1 = tok20PiuFreq(aggMostFreq1)
     aggMostFreq2 = tok20PiuFreq(aggMostFreq2)
     print "\n20 aggettivi più frequenti in ordine di frequenza decrescente:"
-    print "\nRECENSIONI POSITIVE:", aggMostFreq1, "\n"
-    print "RECENSIONI NEGATIVE:", aggMostFreq2, "\n"
+    print "\nRECENSIONI POSITIVE:"
+    for aggettivo in aggMostFreq1:
+        print aggettivo[0], aggettivo[1]
+    print "\nRECENSIONI NEGATIVE:"
+    for aggettivo in aggMostFreq2:
+        print aggettivo[0], aggettivo[1]
+    # 20 bigrammi di token più frequenti (no punteggiatura, articoli e congiunzioni)
+    # prendo lista POS e rimuovo punteggiatura, articoli e congiunzioni formando la lista dei bigrammi
+    listaBigr1 = noPuntArtCong(tokensPOS1)
+    listaBigr2 = noPuntArtCong(tokensPOS2)
+    # prendo solo i primi 20 bigrammi
+    lista20Bigr1 = tok20PiuFreq(listaBigr1)
+    lista20Bigr2 = tok20PiuFreq(listaBigr2)
+    print "\n20 bigrammi più frequenti in ordine di frequenza decrescente"
+    print "\nRECENSIONI POSITIVE:\n",
+    for bigramma in lista20Bigr1:
+        print bigramma[0], bigramma[1]
+    print "\nRECENSIONI NEGATIVE:"
+    for bigramma in lista20Bigr2:
+        print bigramma[0], bigramma[1]
 
 main(sys.argv[1], sys.argv[2])
